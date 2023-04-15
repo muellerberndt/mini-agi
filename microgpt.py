cbed1978d9efe2be20e38a9fc4aecd6b885afdbc
Bernhard Mueller <bernhardmueller@multipoarneuron.lan>
2023-04-15 23:39:04 +0700

    Summarize memory in chunks

--- microgpt.py
+++ microgpt.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import textwrap
 import tiktoken
 import openai as o
 from duckduckgo_search import ddg
@@ -9,14 +10,17 @@ from contextlib import redirect_stdout
 
 MODEL = "gpt-4"
 MAX_TOKENS = 8000
-SYSTEM_PROMPT = "You are an autonomous agent who fulfills the users' objective."
+SUMMARIZER_MODEL = "gpt-3.5-turbo"
+SUMMARIZER_CHUNK_SIZE = 2500
+SYSTEM_PROMPT = "You are an autonomous agent who fulfills the user's objective."
 INSTRUCTIONS = '''
-Carefully consider the next command to execute and pass it to the agent. Execute Python code by setting "type" to Python or shell commands by setting "type" to shell.
-All Python code must have an output "print" statement. Do NOT precede shell commands with an exclamation mark. Use only non-interactive shell commands.
+Carefully consider your next command.
+All Python code run with execute_python must have an output "print" statement.
+Use only non-interactive shell commands.
 Respond with "DONE!" when the objective was accomplished.
 Otherwise, respond with a JSON-encoded dict containing one of the commands: execute_python, execute_shell, or web_search.
 
-{"thought": "REASONING", "cmd": "COMMAND", "arg": "ARGUMENT"}
+{"thought": "[REASONING]", "cmd": "[COMMAND]", "arg": "[ARGUMENT]"}
 
 Example:
 
@@ -32,17 +36,27 @@ def append_to_memory(content: str):
     global memory
     memory += f"\n{content}\n"
 
-    num_tokens = len(tiktoken.encoding_for_model(MODEL).encode(memory))
+    num_tokens = len(tiktoken.encoding_for_model(SUMMARIZER_MODEL).encode(memory))
 
     if (num_tokens > MAX_TOKENS / 2):
-        rs = o.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages = [
-                {"role": "user", "content": f"Shorten the following memory chunk of an autonomous agent from a first person perspective, {int(MAX_TOKENS/3)} tokens max."},
-                {"role": "user", "content": f"Do your best to retain all semantic information including tasks performed by the agent, website content, important data points and hyper-links:\n\n{memory}"}, 
-            ])
+        avg_chars_per_token = len(memory) / num_tokens
 
-        memory = rs['choices'][0]['message']['content']
+        chunk_size = int(avg_chars_per_token * SUMMARIZER_CHUNK_SIZE)
+        chunks = textwrap.wrap(memory, chunk_size)
+        summary_size = int(MAX_TOKENS / 2 / len(chunks))
+
+        print(f"Summarizing memory in {len(chunks)} chunks of {chunk_size} characters")
+
+        memory = ""
+
+        for chunk in chunks:
+            rs = o.ChatCompletion.create(
+                model="SUMMARIZER_MODEL",
+                messages = [
+                    {"role": "user", "content": f"Shorten the following memory chunk of an autonomous agent from a first person perspective, {summary_size} tokens max."},
+                    {"role": "user", "content": f"Do your best to retain all semantic information including tasks performed by the agent, website content, important data points and hyper-links:\n\n{chunk}"}, 
+                ])
+            memory += "CHUNK: " + rs['choices'][0]['message']['content']
 
 if __name__ == "__main__":
     while(True):
@@ -67,16 +81,16 @@ if __name__ == "__main__":
             arg = response["arg"]
         except Exception as e:
             print(f"Unable to parse response:\n{response_text}\n")
-            append_to_memory(f"Invalid JSON response. Respond only with the correct JSON format! Error: {str(e)}")
+            append_to_memory(f"Seems like you responded with the wrong JSON format.\nError: {str(e)}")
             continue
 
         _arg = arg.replace("\n", "\\n") if len(arg) < 64 else f"{arg[:64]}...".replace("\n", "\\n") 
         print(f"MicroGPT: {thought}\nCmd: {command}, Arg: \"{_arg}\"")
-        append_to_memory(f"THOUGHT: {thought}\nCMD: {command}\nARG:\n{arg}\nRESULT:")
+        append_to_memory(f"You thought: {thought}\nYour command: {command}\nCmd argument:\n{arg}\nResult:")
         user_input = input('Press enter to perform this action or abort by typing feedback: ')
 
         if (len(user_input) > 0):
-            append_to_memory(f"User feedback: {user_input}")    
+            append_to_memory(f"User feedback: {user_input}")
             continue
         try:
             if (command == "execute_python"):
@@ -90,4 +104,4 @@ if __name__ == "__main__":
             elif command == "web_search":
                 append_to_memory(ddg(arg, max_results=5))
         except Exception as e:
-                append_to_memory(f"Error executing command: {str(e)}")
+                append_to_memory(f"The command returned an error:\n{str(e)}\nYou should fix the command.")
