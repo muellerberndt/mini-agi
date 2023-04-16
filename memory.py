581d566b6e0bc0bc4845587fe49313f856e4873b
Bernhard Mueller <bernhardmueller@Multipolar-Neuron.local>
2023-04-16 13:08:23 +0700

    Pinecone integration

---
+++ memory.py
@@ -0,0 +1,74 @@
+import os
+import openai
+import textwrap
+import tiktoken
+import pinecone
+import uuid
+
+def create_ada_embedding(data):
+    return openai.Embedding.create(
+        input=[data],
+        model="text-embedding-ada-002"
+    )["data"][0]["embedding"]
+
+class PineconeMemory():
+
+    def __init__(self):
+        self.summarizer_model = os.getenv("SUMMARIZER_MODEL")
+        self.max_context_size = int(os.getenv("MAX_CONTEXT_SIZE"))
+        self.summarizer_chunk_size = int(os.getenv("SUMMARIZER_CHUNK_SIZE"))
+
+        pinecone.init(
+            api_key=os.getenv("PINECONE_API_KEY"),
+            environment=os.getenv("PINECONE_REGION")
+        )
+
+        if "microgpt" not in pinecone.list_indexes():
+            print("Creating Pinecode index...")
+            pinecone.create_index(
+                "microgpt", dimension=1536, metric="cosine", pod_type="p1"
+            )
+
+        self.index = pinecone.Index("microgpt")
+
+        if os.getenv("CLEAR_DB_ON_START") in ['true', '1', 't', 'y', 'yes']:
+            self.index.delete(deleteAll='true')
+
+
+    def add(self, data: str):
+        vector = create_ada_embedding(data)
+
+        id = uuid.uuid1()
+
+        self.index.upsert([(str(id), vector, {"data": data})])
+        print(f"Inserting data into memory at index: {id}:\n data: {data}")
+
+    def get_context(self, data, num=5):
+        vector = create_ada_embedding(data)
+        results = self.index.query(
+            vector, top_k=num, include_metadata=True
+        )
+        sorted_results = sorted(results.matches, key=lambda x: x.score)
+        results_list = [str(item["metadata"]["data"]) for item in sorted_results]
+        context = "\n".join(results_list)
+
+        num_tokens = len(tiktoken.encoding_for_model(self.summarizer_model).encode(context))
+
+        # Summarize memory contents if they're too large to fit into the LLM context
+        if (num_tokens > self.max_context_size):
+            avg_chars_per_token = len(context) / num_tokens
+            chunk_size = int(avg_chars_per_token * self.summarizer_chunk_size)
+            chunks = textwrap.wrap(context, chunk_size)
+            summary_size = int(self.max_context_size / len(chunks))
+            context = ""
+
+            for chunk in chunks:
+                rs = openai.ChatCompletion.create(
+                    model=self.summarizer_model,
+                    messages = [
+                        {"role": "user", "content": f"Shorten the following memory chunk of an autonomous agent from a first person perspective, {summary_size} tokens max."},
+                        {"role": "user", "content": f"Do your best to retain all semantic information including tasks performed by the agent, website content, important data points and hyper-links:\n\n{chunk}"}, 
+                    ])
+                context += rs['choices'][0]['message']['content']
+
+        return context
--- microgpt.py
+++ microgpt.py
@@ -1,17 +1,18 @@
+import os
 import sys
 import json
-import textwrap
-import tiktoken
-import openai as o
+import openai
 from duckduckgo_search import ddg
 from io import StringIO
 import subprocess
 from contextlib import redirect_stdout
+from dotenv import load_dotenv
 
-MODEL = "gpt-4"
-MAX_TOKENS = 8000
-SUMMARIZER_MODEL = "gpt-3.5-turbo"
-SUMMARIZER_CHUNK_SIZE = 2500
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+from memory import PineconeMemory
+
 SYSTEM_PROMPT = "You are an autonomous agent who fulfills the user's objective."
 INSTRUCTIONS = '''
 Carefully consider your next command.
@@ -19,51 +20,33 @@ All Python code run with execute_python must have an output "print" statement.
 Use only non-interactive shell commands.
 When you have achieved the objective and do not need to perform any more actions, repond only with OBJECTIVE ACHIEVED
 Otherwise, respond with a JSON-encoded dict containing one of the commands: execute_python, execute_shell, or web_search.
-
 {"thought": "[REASONING]", "cmd": "[COMMAND]", "arg": "[ARGUMENT]"}
-
 Example:
-
 {"First, I will search for websites relevant to salami pizza.", "cmd": "web_search", "arg": "salami pizza"}
-
 IMPORTANT: ALWAYS RESPOND ONLY WITH THIS EXACT JSON FORMAT. DOUBLE-CHECK YOUR RESPONSE TO MAKE SURE IT CONTAINS VALID JSON. DO NOT INCLUDE ANY EXTRA TEXT WITH THE RESPONSE.
 '''
 
-objective = sys.argv[1]
-memory = ""
-
-def append_to_memory(content: str):
-    global memory
-    memory += f"\n{content}\n"
-
-    num_tokens = len(tiktoken.encoding_for_model(SUMMARIZER_MODEL).encode(memory))
-
-    if (num_tokens > MAX_TOKENS / 2):
-        avg_chars_per_token = len(memory) / num_tokens
-
-        chunk_size = int(avg_chars_per_token * SUMMARIZER_CHUNK_SIZE)
-        chunks = textwrap.wrap(memory, chunk_size)
-        summary_size = int(MAX_TOKENS / 2 / len(chunks))
-        memory = ""
-
-        for chunk in chunks:
-            rs = o.ChatCompletion.create(
-                model=SUMMARIZER_MODEL,
-                messages = [
-                    {"role": "user", "content": f"Shorten the following memory chunk of an autonomous agent from a first person perspective, {summary_size} tokens max."},
-                    {"role": "user", "content": f"Do your best to retain all semantic information including tasks performed by the agent, website content, important data points and hyper-links:\n\n{chunk}"}, 
-                ])
-            memory += "CHUNK: " + rs['choices'][0]['message']['content']
-
 if __name__ == "__main__":
+
+    model = os.getenv("MODEL")
+    objective = sys.argv[1]
+    memory = PineconeMemory()
+    context = objective
+    thought = "I awakened moments ago."
+
     while(True):
-        print(f"Prompting {MODEL}...")
-        rs = o.ChatCompletion.create(
-            model=MODEL,
+        print(f"Prompting {model}...")
+
+        context = memory.get_context(f"{objective}, {thought}")
+
+        print("CONTEXT: " + context)
+
+        rs = openai.ChatCompletion.create(
+            model=model,
             messages = [
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": f"OBJECTIVE:{objective}"},
-                {"role": "user", "content": f"MEMORY:\n{memory}"},
+                {"role": "user", "content": f"COMTEXT:\n{context}"},
                 {"role": "user", "content": f"INSTRUCTIONS:\n{INSTRUCTIONS}"},
             ])
 
@@ -79,29 +62,30 @@ if __name__ == "__main__":
             thought = response["thought"]
             command = response["cmd"]
             arg = response["arg"]
+
+            mem = f"Your thought: {thought}\nYour command: {command}\nCmd argument:\n{arg}\nResult:\n"
+
         except Exception as e:
-            print(f"Unable to parse response:\n{response_text}\n")
-            append_to_memory(f"Seems like you responded with the wrong JSON format.\nError: {str(e)}")
+            print(f"Unable to parse response:\n{str(e)}\Retrying...\n")
             continue
 
         _arg = arg.replace("\n", "\\n") if len(arg) < 64 else f"{arg[:64]}...".replace("\n", "\\n")
         print(f"MicroGPT: {thought}\nCmd: {command}, Arg: \"{_arg}\"")
-        append_to_memory(f"You thought: {thought}\nYour command: {command}\nCmd argument:\n{arg}\nResult:")
         user_input = input('Press enter to perform this action or abort by typing feedback: ')
 
         if (len(user_input) > 0):
-            append_to_memory(f"The user responded: {user_input}. Take this comment into consideration.")
+            memory.add(f"{mem}The user responded: {user_input}. Take this comment into consideration.")
             continue
         try:
             if (command == "execute_python"):
                 f = StringIO()
                 with redirect_stdout(f):
                     exec(arg)
-                append_to_memory(f.getvalue())
+                memory.add(f"{mem}{f.getvalue()}")
             elif command == "execute_shell":
                 result = subprocess.run(arg, capture_output=True, shell=True)
-                append_to_memory(f"STDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}\n")
+                memory.add(f"{mem}STDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}")
             elif command == "web_search":
-                append_to_memory(ddg(arg, max_results=5))
+                memory.add(f"{mem}{ddg(arg, max_results=5)}")
         except Exception as e:
-                append_to_memory(f"The command returned an error:\n{str(e)}\nYou should fix the command.")
+                memory.add(f"{mem}The command returned an error:\n{str(e)}\nYou should fix the command.")
