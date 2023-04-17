599556131542386b57c55945e9c017300e04ae1d
Eric Stiens <estiens@users.noreply.github.com>
2023-04-17 16:00:12 -0500

    add postgres backend for memory

--- memory.py
+++ memory.py
@@ -1,41 +1,24 @@
 import os
+import uuid
 import openai
 import textwrap
 import tiktoken
-import pinecone
-import uuid
+from typing import List
 
 def create_ada_embedding(data):
     return openai.Embedding.create(
         input=[data],
         model="text-embedding-ada-002"
     )["data"][0]["embedding"]
-
-class PineconeMemory():
-
+class Memory:
     def __init__(self):
         self.summarizer_model = os.getenv("SUMMARIZER_MODEL")
         self.max_context_size = int(os.getenv("MAX_CONTEXT_SIZE"))
         self.summarizer_chunk_size = int(os.getenv("SUMMARIZER_CHUNK_SIZE"))
 
-        pinecone.init(
-            api_key=os.getenv("PINECONE_API_KEY"),
-            environment=os.getenv("PINECONE_REGION")
-        )
-
-        if "microgpt" not in pinecone.list_indexes():
-            print("Creating Pinecode index...")
-            pinecone.create_index(
-                "microgpt", dimension=1536, metric="cosine", pod_type="p1"
-            )
-
-        self.index = pinecone.Index("microgpt")
-
-        if os.getenv("CLEAR_DB_ON_START") in ['true', '1', 't', 'y', 'yes']:
-            self.index.delete(deleteAll='true')
-
-    def summarize_memory_if_large(self, memory:str, max_tokens:int) -> str:
-        num_tokens = len(tiktoken.encoding_for_model(self.summarizer_model).encode(memory))
+    def summarize_memory_if_large(self, memory: str, max_tokens: int) -> str:
+        num_tokens = len(tiktoken.encoding_for_model(
+            self.summarizer_model).encode(memory))
 
         if num_tokens > max_tokens:
             avg_chars_per_token = len(memory) / num_tokens
@@ -49,14 +32,44 @@ class PineconeMemory():
             for chunk in chunks:
                 rs = openai.ChatCompletion.create(
                     model=self.summarizer_model,
-                    messages = [
+                    messages=[
                         {"role": "user", "content": f"Shorten the following memory chunk of an autonomous agent from a first person perspective, {summary_size} tokens max."},
-                        {"role": "user", "content": f"Do your best to retain all semantic information including tasks performed by the agent, website content, important data points and hyper-links:\n\n{chunk}"}, 
+                        {"role": "user", "content": f"Do your best to retain all semantic information including tasks performed by the agent, website content, important data points and hyper-links:\n\n{chunk}"},
                     ])
                 memory += rs['choices'][0]['message']['content']
-        
+
         return memory
-        
+
+    def add(self, data: str):
+        raise NotImplementedError
+
+    def get_context(self, data, num=5):
+        raise NotImplementedError
+
+
+memory_type = os.getenv("MEMORY_TYPE")
+
+if memory_type == "pinecone":
+    import pinecone
+
+    class PineconeMemory(Memory):
+        def __init__(self):
+            super().__init__()
+            pinecone.init(
+                api_key=os.getenv("PINECONE_API_KEY"),
+                environment=os.getenv("PINECONE_REGION")
+            )
+
+            if "microgpt" not in pinecone.list_indexes():
+                print("Creating Pinecode index...")
+                pinecone.create_index(
+                    "microgpt", dimension=1536, metric="cosine", pod_type="p1"
+                )
+
+            self.index = pinecone.Index("microgpt")
+
+            if os.getenv("CLEAR_DB_ON_START") in ['true', '1', 't', 'y', 'yes']:
+                self.index.delete(deleteAll='true')
 
     def add(self, data: str):
         vector = create_ada_embedding(data)
@@ -71,9 +84,97 @@ class PineconeMemory():
             vector, top_k=num, include_metadata=True
         )
         sorted_results = sorted(results.matches, key=lambda x: x.score)
-        results_list = [str(item["metadata"]["data"]) for item in sorted_results]
+        results_list = [str(item["metadata"]["data"])
+                        for item in sorted_results]
         context = "\n".join(results_list)
 
-        context = self.summarize_memory_if_large(context, self.max_context_size)
+        context = self.summarize_memory_if_large(
+            context, self.max_context_size)
 
         return context
+
+elif memory_type == "postgres":
+    import psycopg2
+    from psycopg2.extras import execute_values
+    import sklearn
+    from sklearn.metrics.pairwise import cosine_similarity
+
+    class PostgresMemory(Memory):
+        def __init__(self):
+            super().__init__()
+
+            self.conn = psycopg2.connect(
+                host=os.getenv("POSTGRES_HOST"),
+                dbname=os.getenv("POSTGRES_DB"),
+                user=os.getenv("POSTGRES_USER"),
+                password=os.getenv("POSTGRES_PASSWORD")
+            )
+
+            self._create_table_if_not_exists()
+
+            if os.getenv("CLEAR_DB_ON_START") in ['true', '1', 't', 'y', 'yes']:
+                self._clear_table()
+
+        def _create_table_if_not_exists(self):
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS memory (
+                        id UUID PRIMARY KEY,
+                        vector float[] NOT NULL,
+                        data TEXT NOT NULL
+                    );
+                    """
+                )
+                self.conn.commit()
+
+        def _clear_table(self):
+            with self.conn.cursor() as cur:
+                cur.execute("DELETE FROM memory;")
+                self.conn.commit()
+
+        def _get_vectors_from_memory(self) -> List:
+            with self.conn.cursor() as cur:
+                cur.execute("SELECT id, vector, data FROM memory;")
+                vectors = cur.fetchall()
+            return vectors
+
+        def add(self, data: str):
+            vector = create_ada_embedding(data)
+
+            id = uuid.uuid1()
+
+            with self.conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO memory (id, vector, data) VALUES (%s, %s, %s);",
+                    (str(id), vector, data)
+                )
+                self.conn.commit()
+
+        def get_context(self, data, num=5):
+            vector = create_ada_embedding(data)
+
+            all_vectors = self._get_vectors_from_memory()
+            all_vectors.sort(key=lambda x: cosine_similarity(
+                [x[1]], [vector])[0][0], reverse=True)
+
+            top_k_vectors = all_vectors[:num]
+            results_list = [str(item[2]) for item in top_k_vectors]
+            context = "\n".join(results_list)
+
+            context = self.summarize_memory_if_large(
+                context, self.max_context_size)
+
+            return context
+
+else:
+    raise ValueError("Invalid MEMORY_TYPE environment variable")
+
+
+def get_memory_instance():
+    if memory_type == "pinecone":
+        return PineconeMemory()
+    elif memory_type == "postgres":
+        return PostgresMemory()
+    else:
+        raise ValueError("Invalid MEMORY_TYPE environment variable")
