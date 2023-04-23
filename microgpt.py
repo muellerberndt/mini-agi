86e46be20f76247c151e5ca5805a7709deaa1bd2
Alaeddine Abdessalem <alaeddine-13@live.fr>
2023-04-23 03:01:45 +0100

    feat: restore same chain of thought technique

--- microgpt.py
+++ microgpt.py
@@ -11,6 +11,7 @@ scraping websites, and conversing with users.
 
 import os
 import sys
+import re
 import subprocess
 import platform
 from io import StringIO
@@ -19,109 +20,55 @@ from pathlib import Path
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from langchain import OpenAI
 from termcolor import colored
 import openai
 from duckduckgo_search import ddg
 from spinner import Spinner
 from thinkgpt.llm import ThinkGPT
-from thinkgpt.gpt_select import SelectChain
 
 operating_system = platform.platform()
-
-def get_agent(model="gpt-4"):
-    select_llm = OpenAI(model_name="gpt-3.5-turbo", verbose=False)
-    select_chain = SelectChain.from_llm(llm=select_llm, select_examples=SELECT_TOOL_EXAMPLES, verbose=False)
-    agent = ThinkGPT(model_name=model, select_chain=select_chain, verbose=False)
-    return agent
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 DEBUG = os.getenv("DEBUG") in ['true', '1', 't', 'y', 'yes']
 
-tools = ['execute_python', 'execute_shell', 'read_file', 'web_search', 'web_scrape', 'talk_to_user',  'done']
-question = "Which command would you use for to achieve the following task ? Task: {task}. Choose 'done' command *if* " \
-           "the objective was achieved."
+PROMPT = f"You are an autonomous agent running on {operating_system}." + '''
+OBJECTIVE: {objective}
+CONTEXT: {context}
+Carefully consider your next command.
+Supported commands are: execute_python, execute_shell, read_file, web_search, web_scrape, talk_to_user, or done
+The mandatory response format is:
 
+<r>[YOUR_REASONING]</r><c>[COMMAND]</c>
+[ARGUMENT]
 
-SELECT_TOOL_EXAMPLES = [
-    {
-        "question": question.format(task=task),
-        "options_text": '\n'.join(tools),
-        "answer": answer
-    }
-    for task, answer in [
-        ("Search for websites relevant to salami pizza.", "web_search"),
-        ("Scrape information about Apples.", 'web_scrape'),
-        ("I need to ask the user for guidance.", 'talk_to_user'),
-        (">Write 'Hello, world!' to file", 'execute_python')
-    ]
-]
-SYSTEM_PROMPT = f"You are an autonomous agent running on {operating_system}.\n"
-
-NEXT_THOUGHT_PROMPT = SYSTEM_PROMPT + """
-Given an objective and previous interactions with the user, what is your next thought ? 
-Only generate one thought like the provided examples and limit it to one sentence.
-This is the previous memory:
-{context}
-
-Say I'm done if the objective was achieved based.
-Example thoughts:
-Search for websites relevant to salami pizza.
-
-Scrape information about Apples.
-
-What is URL of Domino\\'s Pizza API
-
-Write \\'Hello, world!\\' to file
-
-Now what is your response for this objective: {objective}
-"""
-
-NEXT_ARG_PROMPT = SYSTEM_PROMPT + "You want to achieve the following objective: {objective}\n" + """
-What is the argument that you would pick for the following thought and command ?
-
-Your response may have multiple lines if the command is execute_python.
-if the command is execute_shell, use only non-interactive shell commands.
+ARGUMENT may have multiple lines if the argument is Python code.
+Use only non-interactive shell commands.
 Python code run with execute_python must end with an output "print" statement.
+Send a separate "done" command *after* the objective was achieved.
+RESPOND WITH PRECISELY ONE THOUGHT/COMMAND/ARG COMBINATION.
 DO NOT CHAIN MULTIPLE COMMANDS.
 DO NOT INCLUDE EXTRA TEXT BEFORE OR AFTER THE COMMAND.
-This is the previous context: {context}
-
 
 Examples:
 
-THOUGHT: Search for websites relevant to salami pizza.
-COMMAND: web_search
-RESPONSE: 
+<r>Search for websites relevant to salami pizza.</r><c>web_search</c>
 salami pizza
 
-THOUGHT: Scrape information about Apples.
-COMMAND: web_scrape
-RESPONSE: 
+<r>Scrape information about Apples.</r><c>web_scrape</c>
 https://en.wikipedia.org/wiki/Apple
 
-THOUGHT: I need to ask the user for guidance.
-COMMAND: talk_to_user
-RESPONSE: 
+<r>I need to ask the user for guidance.</r><c>talk_to_user</c>
 What is URL of Domino's Pizza API?
 
-THOUGHT: Write 'Hello, world!' to file
-COMMAND: execute_python
-RESPONSE:
+<r>Write 'Hello, world!' to file</r><c>execute_python</c>
 with open('hello_world.txt', 'w') as f:
     f.write('Hello, world!')
-    
-Your reply now:
-THOUGHT: {thought}
-COMMAND: {command}
-RESPONSE:
-"""
+'''
 
 if __name__ == "__main__":
 
-    model = os.getenv("MODEL")
-    agent = get_agent(model)
+    agent = ThinkGPT(model_name=os.getenv("MODEL"), verbose=False)
 
     if len(sys.argv) != 2:
         print("Usage: microgpt.py <objective>")
@@ -149,8 +96,7 @@ if __name__ == "__main__":
 
     while True:
         context = agent.remember(f"{objective}, {thought}", limit=5, sort_by_order=True)
-        context = "\n".join(context)
-        context = agent.chunked_summarize(context, max_tokens=int(os.getenv("MAX_CONTEXT_SIZE")))
+        context = agent.chunked_summarize("\n".join(context), max_tokens=int(os.getenv("MAX_CONTEXT_SIZE")))
 
         if DEBUG:
             print(f"CONTEXT:\n{context}")
@@ -158,49 +104,56 @@ if __name__ == "__main__":
         with Spinner():
 
             try:
-                thought = agent.predict(prompt=NEXT_THOUGHT_PROMPT.format(operating_system=operating_system, context=context, objective=objective))
-                command = agent.select(question.format(task=thought), options=tools)
+                response_text = agent.predict(prompt=PROMPT.format(context=context, objective=objective))
 
             except openai.error.InvalidRequestError as e:
                 if 'gpt-4' in str(e):
                     print("Prompting the gpt-4 model failed. Falling back to gpt-3.5-turbo")
-                    agent = get_agent(model='gpt-3.5-turbo')
-
+                    agent = ThinkGPT(model_name='gpt-3.5-turbo', verbose=False)
                     continue
                 print("Error accessing the OpenAI API: " + str(e))
                 sys.exit(0)
 
 
+        if DEBUG:
+            print(f"RAW RESPONSE:\n{response_text}")
+
         try:
+            res_lines = response_text.split("\n")
+            PATTERN = r'<(r|c)>(.*?)</(r|c)>'
+            matches = re.findall(PATTERN, res_lines[0])
+
+            thought = matches[0][1]
+            command = matches[1][1]
+
             if command == "done":
                 print("Objective achieved.")
                 sys.exit(0)
-            elif command is None:
-                continue
-            else:
-                argument = agent.predict(prompt=NEXT_ARG_PROMPT.format(objective=objective, context=context, thought=thought, command=command))
-                if DEBUG:
-                    print(f"Thought:\n{thought}\nCommand:\n{command}\nArg:\n{argument}")
 
+            # Account for GPT-3.5 sometimes including an extra "done"
+            if "done" in res_lines[-1]:
+                res_line = res_lines[:-1]
+
+            arg = "\n".join(res_lines[1:])
 
             # Remove unwanted code formatting backticks
-            argument = argument.replace("```", "")
+            arg = arg.replace("```", "")
 
             mem = f"Your thought: {thought}\nYour command: {command}"\
-                f"\nCmd argument:\n{argument}\nResult:\n"
+                f"\nCmd argument:\n{arg}\nResult:\n"
 
         except Exception as e:
             print(colored("Unable to parse response. Retrying...\n", "red"))
             continue
 
         if command == "talk_to_user":
-            print(colored(f"MicroGPT: {argument}", 'cyan'))
+            print(colored(f"MicroGPT: {arg}", 'cyan'))
             user_input = input('Your response: ')
             agent.memorize(f"{mem}The user responded with: {user_input}.")
             continue
 
-        _argument = argument.replace("\n", "\\n") if len(argument) < 64 else f"{argument[:64]}...".replace("\n", "\\n")
-        print(colored(f"MicroGPT: {thought}\nCmd: {command}, Arg: \"{_argument}\"", "cyan"))
+        _arg = arg.replace("\n", "\\n") if len(arg) < 64 else f"{arg[:64]}...".replace("\n", "\\n")
+        print(colored(f"MicroGPT: {thought}\nCmd: {command}, Arg: \"{_arg}\"", "cyan"))
         user_input = input('Press enter to perform this action or abort by typing feedback: ')
 
         if len(user_input) > 0:
@@ -211,15 +164,15 @@ if __name__ == "__main__":
             if command == "execute_python":
                 _stdout = StringIO()
                 with redirect_stdout(_stdout):
-                    exec(argument)
+                    exec(arg)
                 agent.memorize(f"{mem}{_stdout.getvalue()}")
             elif command == "execute_shell":
-                result = subprocess.run(argument, capture_output=True, shell=True, check=False)
+                result = subprocess.run(arg, capture_output=True, shell=True, check=False)
                 agent.memorize(f"{mem}STDOUT:\n{result.stdout}\nSTDERR:\n{result.stderr}")
             elif command == "web_search":
-                agent.memorize(f"{mem}{ddg(argument, max_results=5)}")
+                agent.memorize(f"{mem}{ddg(arg, max_results=5)}")
             elif command == "web_scrape":
-                with urlopen(argument) as response:
+                with urlopen(arg) as response:
                     html = response.read()
 
                 response_text = agent.chunked_summarize(
@@ -232,7 +185,7 @@ if __name__ == "__main__":
 
                 agent.memorize(f"{mem}{response_text}")
             elif command == "read_file":
-                with open(argument, "r") as f:
+                with open(arg, "r") as f:
                     file_content = agent.chunked_summarize(f.read(), max_memory_item_size)
                 agent.memorize(f"{mem}{file_content}")
         except Exception as e:
