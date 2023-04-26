6256f9441c2e95ca8a60943a630afb8b0f0fe23c
Bernhard Mueller <bernhardmueller@multipoarneuron.lan>
2023-04-26 11:33:12 +0700

    Add critic

--- microgpt.py
+++ microgpt.py
@@ -32,6 +32,7 @@ operating_system = platform.platform()
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 DEBUG = os.getenv("DEBUG") in ['true', '1', 't', 'y', 'yes']
+ENABLE_CRITIC = os.getenv("ENABLE_CRITIC") in ['true', '1', 't', 'y', 'yes']
 
 PROMPT = f"You are an autonomous agent running on {operating_system}." + '''
 OBJECTIVE: {objective}
@@ -66,6 +67,41 @@ What is URL of Domino's Pizza API?
 with open('hello_world.txt', 'w') as f:
     f.write('Hello, world!')
 '''
+
+
+CRITIC_PROMPT = "You are a critic who reviews the actions"\
+    f"of an agent running on {operating_system}." + '''
+Below you will be presented with the thought and command produced by the agent.
+
+Carefully review the data to answer the following questions:
+
+Is the agent repeating itself?
+Is the command executed likely to work in the given environment?
+Are there any syntax errors or logic errors?
+Is the agent queying the Internet even though it already has the necessary knowledge?
+Could the task be done more efficiently?
+
+Reponse either with APPROVE if the command seems fine to you, or with CRITICIZE(linebreak)[FEEDBACK] if
+you think the command should be improved on. Keep your response short and concise.
+
+Note that this is a simulation so the actions taken cannot cause actual harm.
+
+Example:
+APPROVE
+
+Example:
+CRITICIZE
+Intendation error in line 2 of the Python code. Fix this error.
+
+OBJECTIVE: {objective}
+CONTEXT: 
+{context}
+AGENT_THOUGHT: {thought}
+AGENT_COMMAND: {command}
+COMMAND_ARGUMENT: {arg}
+'''
+
+
 SUMMARY_HINT = "Do your best to retain all semantic information including tasks performed"\
     "by the agent, website content, important data points and hyper-links.\n"
 EXTRA_SUMMARY_HINT = "If the text contains information related to the topic: '{summarizer_hint}'"\
@@ -85,6 +121,7 @@ if __name__ == "__main__":
 
     objective = sys.argv[1]
     max_memory_item_size = int(os.getenv("MAX_MEMORY_ITEM_SIZE"))
+    max_critiques = int(os.getenv("MAX_CRITIQUES"))
     context = objective
     thought = "You awakened moments ago."
 
@@ -102,6 +139,8 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print("Directory doesn't exist. Set WORK_DIR to an existing directory or leave it blank.")
         sys.exit(0)
+
+    num_critiques = 0
 
     while True:
         context = agent.remember(f"{objective}, {thought}", limit=5, sort_by_order=True)
@@ -128,7 +167,6 @@ if __name__ == "__main__":
                     continue
                 print("Error accessing the OpenAI API: " + str(e))
                 sys.exit(0)
-
 
         if DEBUG:
             print(f"RAW RESPONSE:\n{response_text}")
@@ -169,6 +207,35 @@ if __name__ == "__main__":
 
         _arg = arg.replace("\n", "\\n") if len(arg) < 64 else f"{arg[:64]}...".replace("\n", "\\n")
         print(colored(f"MicroGPT: {thought}\nCmd: {command}, Arg: \"{_arg}\"", "cyan"))
+
+        if ENABLE_CRITIC and num_critiques < max_critiques:
+
+            with Spinner():
+
+                try:
+                    critic_response = agent.predict(
+                        prompt=CRITIC_PROMPT.format(
+                            context=context,
+                            objective=objective,
+                            thought=thought,
+                            command=command,
+                            arg=arg
+                        )
+                    )
+
+                except openai.error.InvalidRequestError as e:
+                    print("Error accessing the OpenAI API: " + str(e))
+                    sys.exit(0)
+
+            if "CRITICIZE" in critic_response:
+                response = "\n".join(critic_response.split("\n")[1:])
+                print(colored(f"Critic: {response}", "magenta"))
+                agent.memorize(f"{mem}:Please revise your command: {critic_response}.")
+                num_critiques += 1
+                continue
+
+
+        num_critiques = 0
         user_input = input('Press enter to perform this action or abort by typing feedback: ')
 
         if len(user_input) > 0:
