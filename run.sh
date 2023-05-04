0da9c13169140027793702b5c11f7db27e651093
bythefire <bythefire@null.dev>
2023-05-04 17:38:15 -0400

    run the mini-agi docker image in a container

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+test ${#} -gt 1 || { echo "usage: $0 <prompt>"; exit; }
+
+# copy environment into working directory
+cp .env ./appdata
+
+# launch container
+docker run -it \
+  -v ./appdata:/app \
+  mini-agi ./env.sh "${*}"
+
