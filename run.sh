d2add8f18caf96934a2d193583720cfc9b89451b
Bernhard Mueller <bernhard.mueller11@gmail.com>
2023-06-14 12:47:54 +0700

    Docker run.sh fixes

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 test ${#} -gt 0 || { echo "usage: $0 <prompt>"; exit; }
 
+mkdir -p ./appdata
+
 # copy environment into working directory
 cp .env ./appdata
 
 # launch container
 docker run -it \
-  -v ./appdata:/app \
+  -v $(pwd)/appdata:/app \
   mini-agi ./env.sh "${*}"
 
